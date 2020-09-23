ed0db7ea5376f8691891c9eab144eff230e467a9
tangzy <tzysz1997@163.com>
2020-09-23 13:53:28 +0800

    添加CodeGenerator代码生成器测试部分--tangzy

---
+++ src/main/java/com/example/demo/tools/CodeGen.java
@@ -0,0 +1,134 @@
+package com.example.demo.tools;
+
+import com.baomidou.mybatisplus.core.exceptions.MybatisPlusException;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.InjectionConfig;
+import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+import com.baomidou.mybatisplus.generator.config.GlobalConfig;
+import com.baomidou.mybatisplus.generator.config.PackageConfig;
+import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+import com.baomidou.mybatisplus.generator.config.rules.DateType;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
+
+public class CodeGen {
+    /**
+     * <p>
+     * 读取控制台内容
+     * </p>
+     */
+    public static String scanner(String tip) {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder help = new StringBuilder();
+        help.append("请输入" + tip + "：");
+        System.out.println(help.toString());
+        if (scanner.hasNext()) {
+            String ipt = scanner.next();
+            if (StringUtils.isNotEmpty(ipt)) {
+                return ipt;
+            }
+        }
+        throw new MybatisPlusException("请输入正确的" + tip + "！");
+    }
+
+    public static void main(String[] args) {
+        // 代码生成器
+        AutoGenerator mpg = new AutoGenerator();
+
+        // 全局配置
+        GlobalConfig gc = new GlobalConfig();
+        String projectPath = System.getProperty("user.dir");
+        gc.setOutputDir(projectPath + "/src/main/java");
+        gc.setAuthor("tangzy");
+        gc.setOpen(false);
+        //实体属性 Swagger2 注解
+        gc.setSwagger2(true);
+        mpg.setGlobalConfig(gc);
+        gc.setDateType(DateType.ONLY_DATE);
+
+        // 数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setUrl("jdbc:mysql://localhost:3306/dcim_tzy?useUnicode=true&characterEncoding=UTF-8&useSSL=false");
+        // dsc.setSchemaName("public");
+        dsc.setDriverName("com.mysql.jdbc.Driver");
+        dsc.setUsername("root");
+        dsc.setPassword("999999");
+        mpg.setDataSource(dsc);
+
+        // 包配置
+        PackageConfig pc = new PackageConfig();
+        pc.setModuleName(scanner("模块名"));
+        pc.setParent("com.example");
+        mpg.setPackageInfo(pc);
+
+        // 自定义配置
+        InjectionConfig cfg = new InjectionConfig() {
+            @Override
+            public void initMap() {
+                // to do nothing
+            }
+        };
+
+        // 如果模板引擎是 freemarker
+//        String templatePath = "/templates/mapper.xml.ftl";
+//        // 如果模板引擎是 velocity
+//        // String templatePath = "/templates/mapper.xml.vm";
+//
+//        // 自定义输出配置
+//        List<FileOutConfig> focList = new ArrayList<>();
+//        // 自定义配置会被优先输出
+//        focList.add(new FileOutConfig(templatePath) {
+//            @Override
+//            public String outputFile(TableInfo tableInfo) {
+//                // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
+//                return projectPath + "/src/main/resources/mapper/" + pc.getModuleName()
+//                        + "/" + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
+//            }
+//        });
+//
+//        cfg.setFileOutConfigList(focList);
+//        mpg.setCfg(cfg);
+//
+//        // 配置模板
+//        TemplateConfig templateConfig = new TemplateConfig();
+//
+//        // 配置自定义输出模板
+//        //指定自定义模板路径，注意不要带上.ftl/.vm, 会根据使用的模板引擎自动识别
+//        // templateConfig.setEntity("templates/entity2.java");
+//        // templateConfig.setService();
+//        // templateConfig.setController();
+//
+//        templateConfig.setXml(null);
+        mpg.setTemplate(null);
+
+        // 策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        // 数据库表映射到实体的命名策略
+        strategy.setNaming(NamingStrategy.underline_to_camel);
+        //数据库表字段映射到实体的命名策略, 未指定按照 naming 执行
+        strategy.setColumnNaming(NamingStrategy.underline_to_camel);
+        // 表前缀,如果设置，则生成对象全部不带前缀，暂时不加
+        strategy.setTablePrefix("h_");
+//        strategy.setSuperEntityClass("com.baomidou.ant.common.BaseEntity");
+        strategy.setEntityLombokModel(true);
+        // 生成 @RestController 控制器
+        strategy.setRestControllerStyle(true);
+        //自定义继承的Controller类全称，带包名
+        strategy.setSuperControllerClass("com.example.demo.controller.TestController");
+        strategy.setInclude(scanner("表名，多个英文逗号分割").split(","));
+        // 自定义继承公共类主键
+        strategy.setSuperEntityClass("com.example.demo.bean.TestModel");
+        // 公共父类已有字段
+//        strategy.setSuperEntityColumns(new String[]{"pkid", "flag", "creationTime","createdBy", "updateTime", "updatedBy"});
+        // 驼峰转连字符
+        strategy.setControllerMappingHyphenStyle(true);
+        // 逻辑删除属性名称
+        strategy.setLogicDeleteFieldName("flag");
+        mpg.setStrategy(strategy);
+        // 模板引擎
+//        mpg.setTemplateEngine(new FreemarkerTemplateEngine());
+        mpg.execute();
+    }
+}
